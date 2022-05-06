611820eadc923a75e9f50065ab93d5065edcc0d9
celineooi <celine.y.l.ooi@gmail.com>
2022-05-06 15:38:49 +0800

    add droplist

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -11,116 +11,116 @@ import Snackbar from "@mui/material/Snackbar";
 import Alert from "@mui/material/Alert";
 import CircularProgress from "@mui/material/CircularProgress";
 import { ButtonRed } from "../Globals";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import DropdownList from '../components/DropdownList'
 
-export default class DynamicTable extends BasePageComponent  {
-  constructor(props) {
-    super(props);
+export default class Add extends BasePageComponent {
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      add_Data: "",
-      isloading: false,
-      message: "",
-      items: [],
-    };
-  }
+        this.state = {
+            add_Data: "",
+            isloading: false,
+            message: "",
+            items: [],
+        };
+    }
 
-  updateMessage(event) {
-    this.setState({
-      message: event.target.value,
-    });
-  }
+    updateMessage(event) {
+        this.setState({
+            message: event.target.value,
+        });
+    }
 
-  handleClick() {
-    var items = this.state.items;
+    handleClick() {
+        var items = this.state.items;
 
-    items.push(this.state.message);
+        items.push(this.state.message);
 
-    this.setState({
-      items: items,
-      message: "",
-    });
-  }
+        this.setState({
+            items: items,
+            message: "",
+        });
+    }
 
-  handleItemChanged(i, event) {
-    var items = this.state.items;
-    items[i] = event.target.value;
+    handleItemChanged(i, event) {
+        var items = this.state.items;
+        items[i] = event.target.value;
 
-    this.setState({
-      items: items,
-    });
-  }
+        this.setState({
+            items: items,
+        });
+    }
 
-  handleItemDeleted(i) {
-    var items = this.state.items;
+    handleItemDeleted(i) {
+        var items = this.state.items;
 
-    items.splice(i, 1);
+        items.splice(i, 1);
 
-    this.setState({
-      items: items,
-    });
-  }
+        this.setState({
+            items: items,
+        });
+    }
 
-  render() {
-    return super.render(
-      <div className="Add">
-        <div className="container">
-          <div className="card">
-            <div className="card-body">
-              <table className="add">
-                <p className="title">Add Expense</p>
+    render() {
+        return super.render(
+            <div className="Add">
+                <div className="container">
+                    <div className="card">
+                        <div className="card-body">
+                            <table className="add">
 
-                <div className="form">
-                  <div className="form-group">
-                    <label htmlFor="add_Data">Add your expenses</label>
-                    <FormControl className="form-control" variant="outlined">
-                      <OutlinedInput
-                        id="add_Data"
-                        type="add_Data"
-                        name="add_Data"
-                        className="input"
-                        variant="outlined"
-                        fullWidth
-                        value={this.state.add_Data}
-                        onChange={this.textChange}
-                      />
-                    </FormControl>
-                  </div>
-                  {this.state.isloading === false && (
-                    <ButtonRed
-                      variant="contained"
-                      color="primary"
-                      className="submitBtn"
-                      onClick={this.add_Data}
-                    >
-                      Log in
-                    </ButtonRed>
-                  )}
-                  {this.state.isloading === true && (
-                    <ButtonRed
-                      variant="contained"
-                      color="primary"
-                      className="loading-button"
-                      disabled={true}
-                    >
-                      <CircularProgress className="loading" />
-                      Add Expense
-                    </ButtonRed>
-                  )}
+
+                                <div className="form">
+                                    <div className="form-group">
+                                        <p className="title">Add New Business Expenses</p>
+
+                                        <label htmlFor="chooseProject">Choose your project</label>
+                                        <div className="container_drop">
+                                            <DropdownList />
+                                        </div>
+                                        <label htmlFor="add_Data">Add your expenses</label>
+                                        <FormControl className="form-control" variant="outlined">
+                                            <OutlinedInput
+                                                id="add_Data"
+                                                type="add_Data"
+                                                name="add_Data"
+                                                className="input"
+                                                variant="outlined"
+                                                fullWidth
+                                                value={this.state.add_Data}
+                                                onChange={this.textChange}
+                                            />
+                                        </FormControl>
+                                    </div>
+                                    {this.state.isloading === false && (
+                                        <ButtonRed
+                                            variant="contained"
+                                            color="primary"
+                                            className="submitBtn"
+                                            onClick={this.add_Data}
+                                        >
+                                            Add New Expense
+                                        </ButtonRed>
+                                    )}
+                                    {this.state.isloading === true && (
+                                        <ButtonRed
+                                            variant="contained"
+                                            color="primary"
+                                            className="loading-button"
+                                            disabled={true}
+                                        >
+                                            <CircularProgress className="loading" />
+                                            Add New Expense
+                                        </ButtonRed>
+                                    )}
+                                </div>
+                            </table>
+                            <hr />
+                        </div>
+                    </div>
                 </div>
-
-                {/* <tbody>{this.renderRows()}</tbody> */}
-              </table>
-              <hr />
-              <input
-                type="text"
-                value={this.state.message}
-                onChange={this.updateMessage.bind(this)}
-              />
-              <button onClick={this.handleClick.bind(this)}></button>
             </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
