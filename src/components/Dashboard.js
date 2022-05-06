95b33b15d39b86ef9c9d6e47206b9c7e2d6e7139
Hiyoshi <nobuaki.hiyoshi@autodesk.com>
2022-05-06 11:11:26 +0800

    Removed redundant files
    Added Dashboard page js file to start working on pulling data.

---
+++ src/components/Dashboard.js
@@ -0,0 +1,9 @@
+import "./Dashboard.scss";
+
+const dummyData = {};
+
+function Dashboard() {
+  return <div>Data testing</div>;
+}
+
+export default Dashboard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,19 @@
-import BasePageComponent from '../components/BasePageComponent';
-import "./Home.scss"
+import BasePageComponent from "../components/BasePageComponent";
+import "./Home.scss";
 
+import Dashboard from "../components/Dashboard";
 
 export default class Home extends BasePageComponent {
-    constructor(props) {
-        super(props)
-        this.state = {
-        }
-    }
-    
-    render() {
-        return super.render(
-            <div className="Home">
-                <p className="">Body</p>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return super.render(
+      <div className="Home">
+        <Dashboard />
+      </div>
+    );
+  }
 }
